1a7cdb8e0677e2b8763aa168e8ffd4055acb49ad
antonio.agasantos05 <aagomes0505@gmail.com>
2024-09-11 23:07:18 -0300

    Criação menu e home

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.CadastroActivity
@@ -18,10 +19,22 @@ class LoginActivity : AppCompatActivity() {
         // Encontre o TextView que leva à tela de cadastro
         val cadastroLink: TextView = findViewById(R.id.tv_cadastro)
 
-        // Configure o listener de clique para o TextView
+        // Botão de login
+        val loginButton: Button = findViewById(R.id.btn_login)
+
+        // Configure o listener de clique para o TextView de cadastro
         cadastroLink.setOnClickListener {
             // Cria um Intent para iniciar a CadastroActivity
             val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Configure o listener de clique para o botão de login
+        loginButton.setOnClickListener {
+            // Aqui você pode adicionar a lógica de verificação do login (validação de credenciais)
+
+            // Redireciona para a MainActivity após o login bem-sucedido
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
